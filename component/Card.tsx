80920fabb45bc9beb047d944fd689680ddf2edf2
Kalib Hackin <kalib@torre.co>
2022-02-15 19:39:19 -0500

    added the search results

---
+++ component/Card.tsx
@@ -0,0 +1,28 @@
+import Link from "next/link";
+import styles from "../styles/Card.module.scss";
+
+const Card = ({
+	title,
+	id,
+	principalOrFirstMaker,
+	image
+}:{
+	title: string;
+	id: string;
+	principalOrFirstMaker: string;
+	image: {
+		url: string
+	};
+}) => {
+	return (
+		<Link href={`item/${id}`}>
+			<div className={styles.card}>
+				<img src={image.url} alt={title} />
+				<h2>{title}</h2>
+				<h4>Main Author: {principalOrFirstMaker}</h4>
+			</div>
+		</Link>
+	);
+};
+
+export default Card;
--- pages/search/[word]/index.tsx
+++ pages/search/[word]/index.tsx
@@ -1,6 +1,48 @@
+import axios from "axios";
+import { useRouter } from "next/router";
+import { useEffect, useState } from "react";
+import ReactLoading from 'react-loading';
+import Card from "../../../component/Card";
+import styles from '../../../styles/Search.module.scss';
+
 const SearchWord = () => {
+	const router = useRouter();
+  const word = typeof router.query.word === 'string' && router.query.word;
+	const [results, setResults] = useState([]);
+	const [loading, setLoading] = useState<boolean>(true);
+
+	useEffect(() => {
+		axios
+			.get(`https://www.rijksmuseum.nl/api/en/collection?key=${process.env.NEXT_PUBLIC_API_KEY}&q=${word}&imgonly=true&p=0&ps=12`)
+			.then(resp => {
+				setResults(resp.data.artObjects);
+				setLoading(false);
+			})
+			.catch(error => {
+				console.error(error);
+			})
+	}, []);
+
 	return (
-		<h1>Search</h1>
+		loading
+      ? (
+        <div className={styles.loadingContainer}>
+          <ReactLoading type="bubbles" color="#6E7E85" height={667} width={375} />
+        </div>
+      ):(
+				<div className={styles.container}>
+					<div className={styles.banner}>
+						<h1>Searched: {word}</h1>
+					</div>
+					<div className={styles.resultsArea}>
+						{
+							results.map(({ title, id, principalOrFirstMaker, headerImage }) => (
+								<Card title={title} id={id} principalOrFirstMaker={principalOrFirstMaker} image={headerImage} key={id} />
+							))
+						}
+					</div>
+				</div>
+			)
 	);
 };
 
